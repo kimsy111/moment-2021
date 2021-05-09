5fffbc911ea9dc31120f956ece0daec885096929
kimsy111 <81911849+kimsy111@users.noreply.github.com>
2021-05-09 23:58:19 +0900

    #3.10 Conclusions and clock

--- clock.js
+++ clock.js
@@ -3,12 +3,27 @@ clocktitle = clockContainer.querySelector("h1");
 
 function getTime(){
     const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth()+1;
+    const day =  date.getDate();
+    const dayLabel = date.getDay();
     const minutes = date.getMinutes();
     const hours = date.getHours();
     const seconds = date.getSeconds();
-    clocktitle.innerText=`${hours<10 ? `0${hours}`:hours }:${minutes<10 ? `0${minutes}`:minutes}:${seconds<10 ? `0${seconds}` : seconds}`;
+    clocktitle.innerText=`${year}.${month}.${day} ${dayLabel}
+    ${hours<10 ? `0${hours}`:hours }:${minutes<10 ? `0${minutes}`:minutes}:${seconds<10 ? `0${seconds}` : seconds}`;
 }
-
+/*
+function getTodayLabel() {
+    
+    const week = new Array('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat');
+    
+    const today = new Date().getDay();
+    const todayLabel = week[today];
+    
+    return todayLabel;
+}
+*/
 function init () {
 getTime();
 setInterval(getTime,1000);
