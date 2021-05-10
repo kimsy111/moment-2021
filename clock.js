bc8d1282888b818d9830f92ee8528a87d31dbe73
kimsy111 <81911849+kimsy111@users.noreply.github.com>
2021-05-10 23:54:07 +0900

    header,main,footer setting

--- clock.js
+++ clock.js
@@ -1,30 +1,34 @@
 const clockContainer = document.querySelector(".js-clock"),
-clocktitle = clockContainer.querySelector("h1");
+clocktitle = clockContainer.querySelector("h1"),
+clockday = clockContainer.querySelector("h3");
+
+function getWeek(week){
+    const weeks = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return weeks[week];
+}
+
+function getDay(){
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth();
+    const date = today.getDate();
+    const days = today.getDay();
+    const day = getWeek(days);
+    
+    clockday.innerHTML = `${year}.${month}.${date} ${day}`;
+}
 
 function getTime(){
     const date = new Date();
-    const year = date.getFullYear();
-    const month = date.getMonth()+1;
-    const day =  date.getDate();
-    const dayLabel = date.getDay();
     const minutes = date.getMinutes();
     const hours = date.getHours();
     const seconds = date.getSeconds();
-    clocktitle.innerText=`${year}.${month}.${day} ${dayLabel}
-    ${hours<10 ? `0${hours}`:hours }:${minutes<10 ? `0${minutes}`:minutes}:${seconds<10 ? `0${seconds}` : seconds}`;
+    clocktitle.innerText=`${hours<10 ? `0${hours}`:hours }:${minutes<10 ? `0${minutes}`:minutes}:${seconds<10 ? `0${seconds}` : seconds}`;
 }
-/*
-function getTodayLabel() {
-    
-    const week = new Array('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat');
-    
-    const today = new Date().getDay();
-    const todayLabel = week[today];
-    
-    return todayLabel;
-}
-*/
+
+
 function init () {
+getDay();
 getTime();
 setInterval(getTime,1000);
 }
